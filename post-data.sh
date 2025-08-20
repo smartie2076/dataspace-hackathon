a46fba7c0390b0f2d40a766b45cba8566f5ddd7f
smartie2076 <martha.hoffmann@rl-institut.de>
2025-08-20 15:44:03 +0200

    Use variable, fix post and check post

--- post-data.sh
+++ post-data.sh
@@ -9,9 +9,19 @@ api_key='TrBp2qPJewwjVAjxlIOj'
 login="User: $a, ID: $id_connector, URL Control: $url_controlplane"
 echo $login
 
-curl -X POST https://reinersdatzen-controlplane.hackathon.future-energy-dialog.de \
+asset_name='my-asset'
+url_asset="$url_controlplane/api/management/v3/$asset_name"
+
+echo "Post onto $url_asset"
+
+curl -X POST $url_asset \
     -H "Content-Type: application/json"                         \
-    -H "x-api-key: TrBp2qPJewwjVAjxlIOj"                               \
+    -H "x-api-key: $api_key"                               \
     -d @my-asset.json
+
+echo "Check post"
+
+curl -X GET $url_asset \
+    -H "x-api-key: $api_key"
 
 read -p "Press enter to continue"
