7c9f092fc5211b9d2cfcc4466980f875d39cb365
smartie2076 <martha.hoffmann@rl-institut.de>
2025-08-21 10:07:14 +0200

    Challenge 6 and Challenge 8

---
+++ processing-offis.py
@@ -0,0 +1,23 @@
+import geopandas
+import geopandas as gp
+import matplotlib.pyplot as plt
+
+data = gp.read_file('data.json')
+data = data[data['wind']!=data['wind'].max()]
+print(data.columns)
+ax = data.plot('wind', legend=True, cmap='viridis', title='Windkraft pro Gemeinde in kW')
+ax.set_xlabel("Latitude")
+ax.set_ylabel("Longitude")
+
+plt.savefig('./datamap-offis.png', bbox_inches="tight")
+plt.close()
+
+# Processing
+max_res = data[['key', 'name', 'geometry', 'id']]
+max_res['nennleistung_kW_solar_wind']=data['wind']+data['solar']
+
+ax = max_res.plot('nennleistung_kW_solar_wind', legend=True, cmap='viridis', title='Wind und Solar pro Gemeinde in kW')
+ax.set_xlabel("Latitude")
+ax.set_ylabel("Longitude")
+plt.savefig('./datamap-offis-nennleistung-kW.png', bbox_inches="tight")
+plt.close()
